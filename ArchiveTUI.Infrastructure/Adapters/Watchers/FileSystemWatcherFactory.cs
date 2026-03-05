6a7b0fc7ec1dba7497eb72c7e7ba4b2f2b756251
phentschel <p_hentschel@icloud.com>
2026-03-05 10:24:40 +0100

    Delete ArchiveTUI.Infrastructure directory

--- ArchiveTUI.Infrastructure/Adapters/Watchers/FileSystemWatcherFactory.cs
+++
@@ -1,6 +0,0 @@
-namespace ArchiveTUI.Infrastructure.Adapters.Watchers;
-
-internal sealed class FileSystemWatcherFactory : IFileSystemWatcherFactory
-{
-    public IFileSystemWatcherWrapper Create(string path) => new FileSystemWatcherWrapper(path);
-}
--- ArchiveTUI.Infrastructure/Adapters/Watchers/IFileSystemWatcherFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace ArchiveTUI.Infrastructure.Adapters.Watchers;
-
-internal interface IFileSystemWatcherFactory
-{
-    IFileSystemWatcherWrapper Create(string path);
-}
-
